37530cbfbcfeeb03e9021719168b14aaa3375f1a
Raymond Reuel Wayesu <sseguya256@gmail.com>
2025-07-03 01:26:14 +0300

    Update monitor.R

--- monitor.R
+++ monitor.R
@@ -4,12 +4,12 @@ library(rsconnect)
 # Get usage metrics
 rsconnect::showMetrics(
   appName = "uganda-health-analytics",
-  account = "your-account-name"
+  account = "dnfxdz-raymond-wayesu"
 )
 
 # Show logs
 rsconnect::showLogs(
   appName = "uganda-health-analytics",
-  account = "your-account-name",
+  account = "dnfxdz-raymond-wayesu",
   entries = 100
 )
