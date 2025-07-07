a9d150334750ec43d94307af3a236271010e1f60
Raymond Reuel Wayesu <sseguya256@gmail.com>
2025-07-07 09:10:28 +0300

    Update app.R

--- app.R
+++ app.R
@@ -1,12 +1,15 @@
+# Run setup if packages are missing
+if (!requireNamespace("shiny", quietly = TRUE)) {
+  source("setup.R")
+}
+
 library(shiny)
 
 ui <- fluidPage(
-  h1("Uganda Health Analytics - Test"),
-  p("If you see this, deployment is working!")
+  h1("Test Deployment"),
+  p("If you see this, packages are loading correctly!")
 )
 
-server <- function(input, output, session) {
-  # Empty server
-}
+server <- function(input, output) {}
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
